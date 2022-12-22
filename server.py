7c1aad4bd7888acc55833ee4fcc2afbc480c7c54
EpicKiwi <me@epickiwi.fr>
2022-12-22 21:23:17 +0100

    Fixed hanging problem with multithreading

--- server.py
+++ server.py
@@ -5,59 +5,82 @@ from ipnd.message import IPNDMessage
 from ipnd.service import TCPCLService
 import upcn
 from datetime import datetime, timedelta
+import threading
 
-PERIOD = 10
+PERIOD = 1
 DESTINATION_V4 = "224.0.0.26"
 DESTINATION_V6 = "FF02::1"
 DESTINATION_PORT = 3003
+AAP_PREFIX = "ipcn"
 
 
-with upcn.upcn_sock() as aap:
+def start_beacon_server():
+    with upcn.upcn_sock(AAP_PREFIX+"/server") as aap:
 
-    message = IPNDMessage()
-    message.eid = aap.eid
-    message.period = PERIOD
-    message.sequence_number = 0
-    period_timeout = datetime.now()
+        message = IPNDMessage()
+        message.eid = aap.eid
+        message.period = PERIOD
+        message.sequence_number = 0
+        period_timeout = datetime.now()
 
-    print("Connected to upcn {}".format(aap.eid))
+        with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as sock:
 
-    with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as sock:
+            ttl = struct.pack('b', 1)
+            sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-        ttl = struct.pack('b', 1)
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+            if socket.has_ipv6:
+                sock.setsockopt(socket.IPPROTO_IPV6,
+                                socket.IPV6_MULTICAST_LOOP, 0)
+                print("Emitting on IPv6 {}:{}".format(
+                    DESTINATION_V6, DESTINATION_PORT))
+            else:
+                print("Emitting on IPv4 {}:{}".format(
+                    DESTINATION_V4, DESTINATION_PORT))
 
-        sock.bind(('', DESTINATION_PORT))
+            while True:
 
-        if socket.has_ipv6:
-            group = socket.inet_pton(socket.AF_INET6, DESTINATION_V6)
-            sock.setsockopt(socket.IPPROTO_IP, socket.IPV6_JOIN_GROUP, group)
-            sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, 0)
-            print("Starting on IPv6 {}:{}".format(
-                DESTINATION_V6, DESTINATION_PORT))
-        else:
-            group = socket.inet_pton(socket.AF_INET, DESTINATION_V4)
-            sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, group)
-            print("Starting on IPv4 {}:{}".format(
-                DESTINATION_V4, DESTINATION_PORT))
+                now = datetime.now()
 
-        while True:
+                if now > period_timeout:
 
-            now = datetime.now()
+                    encoded_message = message.encode()
+                    if socket.has_ipv6:
+                        sock.sendto(encoded_message,
+                                    (DESTINATION_V6, DESTINATION_PORT))
+                    else:
+                        sock.sendto(encoded_message,
+                                    (DESTINATION_V4, DESTINATION_PORT))
 
-            if now > period_timeout:
-                encoded_message = message.encode()
-                if socket.has_ipv6:
-                    sock.sendto(encoded_message,
-                                (DESTINATION_V6, DESTINATION_PORT))
+                    message.sequence_number += 1
+                    period_timeout = now + timedelta(seconds=PERIOD)
+
                 else:
-                    sock.sendto(encoded_message,
-                                (DESTINATION_V4, DESTINATION_PORT))
+                    time.sleep((period_timeout - now).seconds)
 
-                message.sequence_number += 1
-                period_timeout = now + timedelta(seconds=PERIOD)
 
-            try:
+def start_beacon_client():
+    with upcn.upcn_sock(AAP_PREFIX+"/client") as aap:
+        with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as sock:
+
+            sock.bind(('', DESTINATION_PORT))
+
+            if socket.has_ipv6:
+                group = socket.inet_pton(socket.AF_INET6, DESTINATION_V6)
+                sock.setsockopt(socket.IPPROTO_IP,
+                                socket.IPV6_JOIN_GROUP, group)
+                sock.setsockopt(socket.IPPROTO_IPV6,
+                                socket.IPV6_MULTICAST_LOOP, 0)
+                print("Listening on IPv6 {}:{}".format(
+                    DESTINATION_V6, DESTINATION_PORT))
+            else:
+                group = socket.inet_pton(socket.AF_INET, DESTINATION_V4)
+                sock.setsockopt(socket.IPPROTO_IP,
+                                socket.IP_ADD_MEMBERSHIP, group)
+                print("Listening on IPv4 {}:{}".format(
+                    DESTINATION_V4, DESTINATION_PORT))
+
+            while True:
+
                 mess = sock.recv(4096)
 
                 try:
@@ -66,5 +89,9 @@ with upcn.upcn_sock() as aap:
                 except Exception as e:
                     print("Invalid ipnd packet received : {}".format(e))
 
-            except TimeoutError:
-                time.sleep(PERIOD / 2)
+
+server_thread = threading.Thread(target=start_beacon_server)
+client_thread = threading.Thread(target=start_beacon_client)
+
+server_thread.start()
+client_thread.start()
--- upcn/__init__.py
+++ upcn/__init__.py
@@ -1,7 +1,8 @@
 import socket
 from .aap import UPCNAAP
 
-def upcn_sock(socket_path: str = "/tmp/upcn.socket"):
+
+def upcn_sock(eid_suffix, socket_path: str = "/tmp/upcn.socket"):
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sock.connect(socket_path)
-    return UPCNAAP(sock)
+    return UPCNAAP(sock, eid_suffix)
