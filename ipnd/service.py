3f2e84b7e8990667a48e43d827f932255a9943f0
EpicKiwi <me@epickiwi.fr>
2022-12-22 22:48:53 +0100

    Added contact definition

--- ipnd/service.py
+++ ipnd/service.py
@@ -15,9 +15,10 @@ DOUBLE_TYPE = 'double'
 STRING_TYPE = 'string'
 BYTES_TYPE = 'bytes'
 
+
 class Service(ABC):
-    
-    def decode_with_offset(bytes: bytes, services_by_tag = None):
+
+    def decode_with_offset(bytes: bytes, services_by_tag=None):
         if services_by_tag is None:
             services_by_tag = DEFAULT_SERVICES
 
@@ -25,21 +26,22 @@ class Service(ABC):
 
         if tag not in services_by_tag:
             return UnknownService.decode_with_offset(bytes)
-        
+
         else:
             return services_by_tag[tag].decode_with_offset(bytes)
 
     @abstractmethod
     def encode(self) -> bytes:
         pass
-    
+
     def __bytes__(self) -> bytes:
         return self.encode()
+
 
 class PrimitiveService:
 
     value = None
-    type:str = None
+    type: str = None
 
     def __init__(self, value, type=None):
         self.value = value
@@ -53,50 +55,52 @@ class PrimitiveService:
 
         if self.type == BOOL_TYPE:
             tag = 0
-            get_val = lambda v: (int(v),)
+            def get_val(v): return (int(v),)
 
         elif self.type == UINT_TYPE:
             tag = 1
-            get_val = lambda v: SDNVUtil.encode(v)
+            def get_val(v): return SDNVUtil.encode(v)
 
         elif self.type == SINT_TYPE:
             tag = 2
-            get_val = lambda v: SDNVUtil.encode(v)
+            def get_val(v): return SDNVUtil.encode(v)
 
         elif self.type == FIXED16_TYPE:
             tag = 3
-            get_val = lambda v: int(v).to_bytes(2, 'big')
+            def get_val(v): return int(v).to_bytes(2, 'big')
 
         elif self.type == FIXED32_TYPE:
             tag = 4
-            get_val = lambda v: int(v).to_bytes(4, 'big')
+            def get_val(v): return int(v).to_bytes(4, 'big')
 
         elif self.type == FIXED64_TYPE:
             tag = 5
-            get_val = lambda v: int(v).to_bytes(8, 'big')
-            
+            def get_val(v): return int(v).to_bytes(8, 'big')
+
         elif self.type == FLOAT_TYPE:
             tag = 6
-            get_val = lambda v: struct.pack("!f", v)
+            def get_val(v): return struct.pack("!f", v)
 
         elif self.type == DOUBLE_TYPE:
             tag = 7
-            get_val = lambda v: struct.pack("!d", v)
+            def get_val(v): return struct.pack("!d", v)
 
         elif self.type == STRING_TYPE:
             tag = 8
-            get_val = lambda v: encode_primitive_array(str(self.value).encode("ascii"))
-            
+            def get_val(v): return encode_primitive_array(
+                str(self.value).encode("ascii"))
+
         elif self.type == BYTES_TYPE:
             tag = 9
-            get_val = lambda v: encode_primitive_array(bytes(self.value))
+            def get_val(v): return encode_primitive_array(bytes(self.value))
 
         else:
-            raise Exception("Unknown primitive service type {}".format(self.type))
+            raise Exception(
+                "Unknown primitive service type {}".format(self.type))
 
         ba.append(tag)
 
-        if self.type != BYTES_TYPE and isinstance(self.value, bytes) :
+        if self.type != BYTES_TYPE and isinstance(self.value, bytes):
             ba.extend(self.value)
         else:
             ba.extend(get_val(self.value))
@@ -141,7 +145,7 @@ class PrimitiveService:
             type = FIXED64_TYPE
             value = int.from_bytes(bytes[offset:offset+8], 'big')
             offset += 8
-            
+
         elif tag == 6:
             type = FLOAT_TYPE
             value = struct.unpack("!f", bytes[offset:offset+4])
@@ -158,27 +162,28 @@ class PrimitiveService:
             offset += num_bytes
             value = bytes[offset:offset+length].decode("ascii")
             offset += length
-            
+
         elif tag == 9:
             type = BYTES_TYPE
             (length, num_bytes) = SDNVUtil.decode(bytes, offset)
             offset += num_bytes
             value = bytes[offset:offset+length]
             offset += length
-        
+
         self = PrimitiveService(value, type)
 
         return (self, offset)
 
     def __bytes__(self) -> bytes:
         return self.encode()
-    
+
     def __repr__(self):
         return """PrimitiveService {{ type={}, value={} }}""".format(self.type, self.value)
 
+
 def encode_primitive_array(a):
     ba = bytearray()
-    
+
     if len(a) == 0:
         ba.append(1)
         ba.append(0)
@@ -188,9 +193,10 @@ def encode_primitive_array(a):
 
     return bytes(ba)
 
+
 class ConstructedService(Service):
-    tag:int = None
-    
+    tag: int = None
+
     def __init__(self, tag: int):
         self.tag = tag
 
@@ -205,7 +211,7 @@ class ConstructedService(Service):
         contentba = bytearray()
         for s in self.get_services():
             contentba.extend(bytes(s))
-        
+
         ba.extend(SDNVUtil.encode(len(contentba)))
         ba.extend(contentba)
 
@@ -215,15 +221,23 @@ class ConstructedService(Service):
     def decode_with_offset(bytes: bytes):
         pass
 
-class TCPCLService(ConstructedService):
-    
-    address:IPv4Address = None
-    port:int = None
+
+class CLAService(ConstructedService):
+
+    @abstractmethod
+    def get_cla_address(self):
+        pass
+
+
+class TCPCLService(CLAService):
+
+    address: IPv4Address = None
+    port: int = None
 
     def __init__(self, address: Union[IPv4Address, IPv6Address, str], port: int):
         if isinstance(address, str):
             address = ip_address(address)
-        
+
         if isinstance(address, IPv4Address):
             self.tag = 64
         elif isinstance(address, IPv6Address):
@@ -233,7 +247,7 @@ class TCPCLService(ConstructedService):
 
         self.address = address
         self.port = port
-    
+
     def get_services(self):
         address_type = None
         if isinstance(self.address, IPv4Address):
@@ -245,14 +259,15 @@ class TCPCLService(ConstructedService):
             PrimitiveService(self.address.packed, type=address_type),
             PrimitiveService(self.port, type=FIXED16_TYPE)
         )
-    
+
     def decode_with_offset(bytes: bytes):
         offset = 1
-        
+
         (length, num_bytes) = SDNVUtil.decode(bytes, offset)
         offset += num_bytes
 
-        (services, num_bytes) = decode_services(2, bytes[offset: offset+length])
+        (services, num_bytes) = decode_services(
+            2, bytes[offset: offset+length])
         offset += num_bytes
 
         address = ip_address(services[0].value)
@@ -261,9 +276,18 @@ class TCPCLService(ConstructedService):
         self = TCPCLService(address, port)
 
         return (self, offset)
-    
+
+    def get_cla_address(self):
+        if isinstance(self.address, IPv4Address):
+            address = str(self.address)
+        elif isinstance(self.address, IPv6Address):
+            address = "{}".format(self.address)
+
+        return "tcpclv3:{}:{}".format(address, self.port)
+
     def __repr__(self):
         return """TCPCLService {{ address={}, port={} }}""".format(self.address, self.port)
+
 
 class UnknownService(Service):
     def decode_with_offset(bytes: bytes):
@@ -271,7 +295,7 @@ class UnknownService(Service):
 
         self.tag = bytes[0]
         offset = 1
-        
+
         (length, num_bytes) = SDNVUtil.decode(bytes, offset)
         offset += num_bytes
 
@@ -282,25 +306,28 @@ class UnknownService(Service):
     def encode(self) -> bytes:
         ba = bytearray()
         ba.append(self.tag)
-        
+
         ba.extend(SDNVUtil.encode(len(self.buffer)))
         ba.extend(self.buffer)
 
         return bytes(ba)
-    
+
     def __repr__(self):
         return """UnknownService {{ tag={}, length={} }}""".format(self.tag, len(self.buffer))
 
-def decode_services(n_services, bytes, services_by_tag = None):
+
+def decode_services(n_services, bytes, services_by_tag=None):
     offset = 0
     service_list = []
-    
+
     for _ in range(n_services):
-        (service, num_bytes) = Service.decode_with_offset(bytes[offset:], services_by_tag)
+        (service, num_bytes) = Service.decode_with_offset(
+            bytes[offset:], services_by_tag)
         service_list += (service,)
         offset += num_bytes
 
     return (service_list, offset)
+
 
 DEFAULT_SERVICES = {
     0: PrimitiveService,
